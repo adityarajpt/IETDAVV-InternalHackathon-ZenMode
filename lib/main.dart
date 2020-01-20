44c70633c64f506fda74afa81babc12bd142ede5
Tushar Sadhwani <18bit160@ietdavv.edu.in>
2020-01-20 18:28:20 +0530

    added login page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
 
+import './login_page.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
